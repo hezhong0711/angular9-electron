dd148c375c267e827ed320f38f3b1d2e55a14884
HeZhong <zhong.he@thoughtworks.com>
2020-05-19 15:04:06 +0800

    add ng-zorro-antd component for angular style.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angural9-electron');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angural9-electron app is running!');
-  });
 });
